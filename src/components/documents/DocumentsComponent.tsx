44ba5fd1e1ae74b97177cffeccf2311c62a0d786
Jakub Gasiorek <220890@student.pwr.edu.pl>
2021-01-09 09:50:45 +0100

    Implement DataGrid table to display documents list

--- src/components/documents/DocumentsComponent.tsx
+++ src/components/documents/DocumentsComponent.tsx
@@ -1,16 +1,71 @@
 import React, {useEffect, useState} from 'react';
-import {DataGrid, ColDef} from '@material-ui/data-grid';
+import {DataGrid, ColDef, CellParams} from '@material-ui/data-grid';
 
 import './DocumentsComponent.scss';
 import {DocumentListItem} from "./DocumentsService";
 import {downloadDocument, fetchDocuments} from "./DocumentsService";
+import {Button} from "@material-ui/core";
 
 
 const columns: ColDef[] = [
     {field: 'id', headerName: 'ID', width: 70},
-    {field: 'fileName', headerName: 'File name', width: 130},
-    {field: 'uploadedBy', headerName: 'Uploaded by', width: 150},
-]
+    {field: 'name', headerName: 'File name', width: 130},
+    {
+        field: '',
+        headerName: 'Details',
+        disableClickEventBubbling: true,
+        renderCell: (params: CellParams) => {
+            const onClick = () => {
+               // go to documents/params.row.id //react router
+            };
+
+            return <Button variant="contained" color="primary" onClick={onClick}>Details</Button>;
+        }
+    }
+];
+
+const rows = [
+    {
+        id: '1',
+        name: '1.png',
+    },
+    {
+        id: '2',
+        name: '2.png',
+    },
+    {
+        id: '3',
+        name: '3.png',
+    },
+    {
+        id: '4',
+        name: '4.png',
+    },
+    {
+        id: '5',
+        name: '5.png',
+    },
+    {
+        id: '6',
+        name: '1.png',
+    },
+    {
+        id: '7',
+        name: '2.png',
+    },
+    {
+        id: '8',
+        name: '3.png',
+    },
+    {
+        id: '9',
+        name: '4.png',
+    },
+    {
+        id: '10',
+        name: '5.png',
+    },
+];
 
 interface DocumentListProps {
     documents: DocumentListItem[],
@@ -20,34 +75,13 @@ interface DocumentListProps {
 
 const DocumentDetailsView: React.FC<DocumentListProps> = ({documents, selectedDocument, onDocumentSelected}) => (
     <div style={{height: 400, width: '100%'}}>
-        <DataGrid rows={documents} columns={columns} pageSize={10} disableMultipleSelection hideFooterSelectedRowCount onRowSelected={() => onDocumentSelected}/>
+        <DataGrid rows={documents} columns={columns} pageSize={5} disableMultipleSelection hideFooterSelectedRowCount onRowSelected={() => onDocumentSelected}/>
     </div>
 );
 
 export const DocumentsComponent: React.FC<{}> = () => {
     const [selectedDocument, setSelectedDocument] = useState<DocumentListItem | null>(null);
-    const [documentsList, setDocumentList] = useState<DocumentListItem[]>([
-        {
-            id: 1,
-            fileName: '1.png',
-        },
-        {
-            id: 2,
-            fileName: '2.png',
-        },
-        {
-            id: 3,
-            fileName: '3.png',
-        },
-        {
-            id: 4,
-            fileName: '4.png',
-        },
-        {
-            id: 5,
-            fileName: '5.png',
-        }
-    ]);
+    const [documentsList, setDocumentList] = useState<DocumentListItem[]>(rows);
 
     useEffect(() => {
         fetchDocuments().then(data => {
@@ -65,10 +99,10 @@ export const DocumentsComponent: React.FC<{}> = () => {
                     selectedDocument={selectedDocument}
                     onDocumentSelected={(document: DocumentListItem) => setSelectedDocument(document)}/>
             </div>
-            {/*<div className='document--details--view'>*/}
+            {/*<div className='documentComponent--details--view'>*/}
             {/*    {*/}
             {/*        selectedDocument ? (*/}
-            {/*            <DocumentDetailsView document={selectedDocument}/>*/}
+            {/*            <DocumentDetailsView documentComponent={selectedDocument}/>*/}
             {/*        ) : (*/}
             {/*            <div>Please select a bucket</div>*/}
             {/*        )*/}
